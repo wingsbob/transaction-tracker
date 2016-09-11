191aa4283f176e7f0c28cf9ab4a6809d50e3145f
Matthew Bull <matt_wings@hotmail.com>
2016-09-11 22:14:46 +0100

    Added HTTPS support

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
-const argv = require('yargs').argv;
 const express = require('express');
+
+const argv = require('yargs').argv;
+const config = require('../config.json');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 const app = express();
 const apiRouter = require('./routers/apiRouter');
 const contentRouter = require('./routers/contentRouter');
@@ -7,4 +12,15 @@ const contentRouter = require('./routers/contentRouter');
 app.use('/content', contentRouter);
 app.use('/api', apiRouter);
 app.get('/', (req, res) => res.redirect('/content/index.html'));
-app.listen(argv.port || 3000);
+
+if (config.useHTTPS) {
+  const {pfx, key, cert} = Object.assign({}, config.httpsOptions); //Object.assign guarantees we get an object
+
+  http.createServer(app).listen(argv.port || config.port || 3000);
+  https.createServer({
+    pfx: pfx ? fs.readFileSync(pfx) : null,
+    key: pfx ? null : fs.readFileSync(key),
+    cert: pfx ? null : fs.readFileSync(cert)
+  }, app).listen(argv.port || config.port || 3000);
+} else
+  app.listen(argv.port || config.port || 3000);
